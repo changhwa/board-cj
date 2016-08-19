0c15c0a4818a157840ed3d3a69ae73579b41ac92
Herschel (Changhwa Oh) <herschel.alway@daumkakao.com>
2016-08-19 18:48:08 +0900

    Init Project

---
+++ src/main/java/io/narratage/CjApplication.java
@@ -0,0 +1,12 @@
+package io.narratage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CjApplication.class, args);
+    }
+}
